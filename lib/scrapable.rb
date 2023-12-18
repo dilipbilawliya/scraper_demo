0a3e8228cef2eaf3f2caed731aad79a03019c098
Dilip <dilip92Bilawliya@gmail.com>
2023-12-19 00:12:07 +0530

    Task#2 functionality with meta information from the page

--- lib/scrapable.rb
+++ lib/scrapable.rb
@@ -11,7 +11,7 @@ class Scrapable
     doc = Nokogiri::HTML(response.body)
     result = process_fields(doc, fields)
     ScrapableResource.create!(url: url, fields: result)
-
+    
     result
   end
 
@@ -20,8 +20,20 @@ class Scrapable
 
     fields.each do |field_name, selector|
       result[field_name] = doc.css(selector).text.strip
+      result[field_name] = process_meta_fields(doc, selector) if field_name == 'meta'
     end
 
     result
   end
+
+  def self.process_meta_fields(doc, meta_tags)
+    meta_result = {}
+
+    meta_tags.each do |meta_tag|
+      meta_result[meta_tag] = doc.at_css("meta[name='#{meta_tag}']")&.attribute('content')&.value
+    end
+
+    meta_result
+  end
 end
+
